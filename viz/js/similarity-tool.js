f1de3a341df5aafe1746b381c480675560d964c2
anuprulez <anup.rulez@gmail.com>
2017-12-20 19:15:35 +0100

    Update the path

--- viz/js/similarity-tool.js
+++ viz/js/similarity-tool.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     var similarityData = null,
         path = "https://raw.githubusercontent.com/anuprulez/similar_galaxy_tools/master/viz/data/similarity_matrix.json";
-    $.getJSON( "data/similarity_matrix.json", function( data ) {
+    $.getJSON( path, function( data ) {
         var toolIdsTemplate = "";
         // sort the tools in ascending order of their ids
         similarityData = data.sort(function(a, b) {
