61535b2ecc6fa4fb2d240d96c68ee6acee8ca6ff
anuprulez <anup.rulez@gmail.com>
2017-12-21 19:41:27 +0100

    Update json file path

--- viz/js/similarity-tool.js
+++ viz/js/similarity-tool.js
@@ -3,8 +3,8 @@
 $(document).ready(function(){
 
     var similarityData = null,
-        path = "https://raw.githubusercontent.com/anuprulez/similar_galaxy_tools/master/viz/data/similarity_matrix.json";
-    $.getJSON( "data/similarity_matrix_400.json", function( data ) {
+        path = "https://raw.githubusercontent.com/anuprulez/similar_galaxy_tools/master/viz/data/similarity_matrix_400.json";
+    $.getJSON( path, function( data ) {
         var toolIdsTemplate = "";
         // sort the tools in ascending order of their ids
         similarityData = data.sort(function(a, b) {
