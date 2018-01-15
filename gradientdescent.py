d3b16f1c5f763c8b8035d2a033c7fc9c799d8df2
anuprulez <anup.rulez@gmail.com>
2018-01-15 20:06:33 +0100

    Reduce gradient

--- gradientdescent.py
+++ gradientdescent.py
@@ -154,7 +154,7 @@ class GradientDescentOptimizer:
                     uniform_cost_sources.append( mean_uniform_loss )
                     cost_source[ source ] = mean_loss
                     # compute average gradient
-                    gradient = np.dot( tools_score_source, loss )
+                    gradient = np.dot( tools_score_source, loss ) / num_all_tools
                     # gather gradient for a source
                     sources_gradient[ source ] = gradient
                 mean_cost = np.mean( cost_sources )
