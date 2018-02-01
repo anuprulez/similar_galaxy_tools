0e2af43e4482588fadf47bb6b3d1bf5adccea635
anuprulez <anup.rulez@gmail.com>
2018-02-01 18:46:05 +0100

    Update the json link

--- viz/js/similarity-tool.js
+++ viz/js/similarity-tool.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     var similarityData = null,
         list_tool_names = null,
-        path = "https://raw.githubusercontent.com/anuprulez/similar_galaxy_tools/lsi/viz/data/similarity_matrix.json";
+        path = "https://raw.githubusercontent.com/anuprulez/similar_galaxy_tools/master/viz/data/similarity_matrix.json";
     if ( path === "" ) {
         console.error( "Error in loading JSON file" );
         return;
