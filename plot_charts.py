b8c7e4fa306fe4b31f380c6586f0a9d1a261ed7b
anuprulez <anup.rulez@gmail.com>
2018-05-03 19:27:19 +0200

    Update gradient descent

--- plot_charts.py
+++ plot_charts.py
@@ -24,11 +24,11 @@ def read_files( file_path ):
 
 def plot_doc_tokens_mat( file_path_io, file_path_nd, file_path_ht ):
     fig, axes = plt.subplots( nrows=1, ncols=3 )
-    sub_titles = [ "Input \& output", "Name \& description", "Help text" ]
+    sub_titles = [ "Input \& output (a)", "Name \& description (b)", "Help text (c)" ]
     io_tools_tokens = read_files( file_path_io )
     nd_tools_tokens = read_files( file_path_nd )
     ht_tools_tokens = read_files( file_path_ht ) 
-    NEW_FONT_SIZE = FONT_SIZE - 2
+    NEW_FONT_SIZE = FONT_SIZE
     io_tokens = list()
     for item in io_tools_tokens:
         a = list()
@@ -76,9 +76,9 @@ def plot_doc_tokens_mat( file_path_io, file_path_nd, file_path_ht ):
         axes[ 1 ].set_xlabel( "100-dimensions", fontsize = NEW_FONT_SIZE )
         axes[ 2 ].set_xlabel( "200-dimensions", fontsize = NEW_FONT_SIZE )
  
-        axes[ 0 ].set_ylabel( "Documents", fontsize = NEW_FONT_SIZE )
-        axes[ 1 ].set_ylabel( "Documents", fontsize = NEW_FONT_SIZE )
-        axes[ 2 ].set_ylabel( "Documents", fontsize = NEW_FONT_SIZE )
+        axes[ 0 ].set_ylabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
+        axes[ 1 ].set_ylabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
+        axes[ 2 ].set_ylabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
     plt.suptitle( "Documents-tokens multi-dimensional matrices" )
     fig.subplots_adjust( right = 0.75 )
     cbar_ax = fig.add_axes( [ 0.8, 0.15, 0.02, 0.7 ] )
@@ -87,19 +87,16 @@ def plot_doc_tokens_mat( file_path_io, file_path_nd, file_path_ht ):
 
 
 def plot_lr_drop( file_path ):
-    data_0_1 = read_files( file_path )
+    tools_lr = read_files( file_path )
     max_len = 0
     lr_drop = list()
-    for item in data_0_1:
-        for gd_iter in data_0_1[ item ]:
-            lr_steps = len( gd_iter )
-            if len( gd_iter ) > max_len:
-                max_len = len( gd_iter )
-                lr_drop = gd_iter
-    plt.plot( lr_drop, marker="*" )
+    for item in tools_lr:
+        lr_drop = tools_lr[ item ]
+        break
+    plt.plot( lr_drop )
     plt.ylabel( 'Gradient descent learning rate' )
     plt.xlabel( 'Iterations' )
-    plt.title( 'Learning rates using backtracking line search' )
+    plt.title( 'Learning rates computed using time-based decay' )
     plt.grid( True )
     plt.show()
 
@@ -109,7 +106,7 @@ def plot_correlation( similarity_matrices, title ):
     NEW_FONT_SIZE = 22
     fig, axes = plt.subplots( nrows=2, ncols=2 )
     sources = [ "input_output", 'name_desc_edam', 'help_text', "optimal" ]
-    titles_fullrank = [ "Input \& output", "Name \& description", "Help text", "Optimal" ]
+    titles_fullrank = [ "Input \& output (a)", "Name \& description (b)", "Help text (c)", "Weighted average (d)" ]
     row_lst = [ [ 0, 1 ], [ 2, 3 ] ]
     for row, axis in enumerate( axes ):
         mat1 = similarity_matrices[ row_lst[ row ][ 0 ] ]
@@ -130,11 +127,11 @@ def plot_correlation( similarity_matrices, title ):
             tick.label.set_fontsize( NEW_FONT_SIZE )
         
         if row == 1:
-            axis[ 0 ].set_xlabel( "Tools", fontsize = NEW_FONT_SIZE )
-            axis[ 1 ].set_xlabel( "Tools", fontsize = NEW_FONT_SIZE )
+            axis[ 0 ].set_xlabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
+            axis[ 1 ].set_xlabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
             
-        axis[ 0 ].set_ylabel( "Tools", fontsize = NEW_FONT_SIZE )
-        axis[ 1 ].set_ylabel( "Tools", fontsize = NEW_FONT_SIZE )
+        axis[ 0 ].set_ylabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
+        axis[ 1 ].set_ylabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
 
     fig.subplots_adjust( right = 0.75 )
     cbar_ax = fig.add_axes( [ 0.8, 0.15, 0.02, 0.7 ] )
@@ -181,7 +178,7 @@ def plot_weights_distribution( file_path, title ):
 
     fig, axes = plt.subplots( nrows=1, ncols=3 )
     sources = [ "input_output", 'name_desc_edam', 'help_text', "optimal" ]
-    sub_titles = [ "Input \& output", "Name \& description", "Help text", "Optimal" ]
+    sub_titles = [ "Input \& output (a)", "Name \& description (b)", "Help text (c)", "Weighted average (d)" ]
     for row, axis in enumerate( axes ):
         axes[ 0 ].plot( weights_io, color = colors_dict[ "input_output" ] )
         axes[ 1 ].plot( weights_nd, color = colors_dict[ "name_desc_edam" ] )
@@ -205,24 +202,68 @@ def plot_weights_distribution( file_path, title ):
         for tick in axes[ 2 ].yaxis.get_major_ticks():
             tick.label.set_fontsize( NEW_FONT_SIZE )
         
-        axes[ 0 ].set_xlabel( "Tools", fontsize = NEW_FONT_SIZE )
-        axes[ 1 ].set_xlabel( "Tools", fontsize = NEW_FONT_SIZE )
-        axes[ 2 ].set_xlabel( "Tools", fontsize = NEW_FONT_SIZE )   
+        axes[ 0 ].set_xlabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
+        axes[ 1 ].set_xlabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )
+        axes[ 2 ].set_xlabel( "Tools (documents)", fontsize = NEW_FONT_SIZE )   
         axes[ 0 ].set_ylabel( "Weights", fontsize = NEW_FONT_SIZE )
     plt.suptitle( title )
     plt.show()
-        
 
 
-#plot_weights_distribution( "data/optimal_weights.json", "Distribution of weights for multiple tools attributes" )
-#plot_doc_tokens_mat( "data/doc_vecs_io.json", "data/doc_vecs_nd.json", "data/doc_vecs_ht.json" )
-#extract_correlation( "data/similarity_scores_sources_optimal.json", "Similarity matrices for multiple tools attributes and an optimal one" )
+def plot_gradient_drop( actual_gd_file_path ):
+    actual_gd = read_files( actual_gd_file_path )
+    tools_len = len( actual_gd )
+    iterations = 100
+
+    actual_io = np.zeros( [ tools_len, iterations ] )
+    actual_nd = np.zeros( [ tools_len, iterations ] )
+    actual_ht = np.zeros( [ tools_len, iterations ] )
+    cumulative_gradient = np.zeros( [ tools_len, iterations ] )
+
+    for index_x, item_y in enumerate( actual_gd ):
+        for index_y, y in enumerate( actual_gd[ item_y ] ):
+            actual_io[ index_x ][ index_y ] = y[ "input_output" ]
+            actual_nd[ index_x ][ index_y ] = y[ "name_desc_edam" ]
+            actual_ht[ index_x ][ index_y ] = y[ "help_text" ]
+
+    for tool_idx in range( tools_len ):
+        for iteration in range( iterations ):
+            cumulative_gradient[ tool_idx ][ iteration ] = np.sqrt( actual_io[ tool_idx ][ iteration ] ** 2 + actual_nd[ tool_idx ][ iteration ] ** 2 + actual_ht[ tool_idx ][ iteration ] ** 2 )
+
+    mean_cumulative_gradient = np.mean( cumulative_gradient, axis = 0 )
+    plt.plot( mean_cumulative_gradient, color='C0' )
+    plt.ylabel( 'Cumulative gradient' )
+    plt.xlabel( 'Iterations' )
+    plt.title( 'Cumulative gradient over iterations for all the tools attributes' )
+    plt.grid( True )
+    plt.show()
+    
+def compute_cost( similarity_data, iterations ):
+    cost_iterations = np.zeros( [ len( similarity_data ) - 1, iterations ] )
+    for index, tool in enumerate( similarity_data ):
+        if "cost_iterations" in tool:
+            cost_iterations[ index ][ : ] = tool[ "cost_iterations" ][ :iterations ]
+    # compute mean cost occurred for each tool across iterations
+    return np.mean( cost_iterations, axis=0 )
+    
+
+def plot_average_cost():
+    max_iter = 100
+    sim_mat = read_files( "data/similarity_matrix.json" )
+    cost = compute_cost( sim_mat, max_iter )
+    plt.plot( cost )
+    plt.ylabel( 'Mean squared error' )
+    plt.xlabel( 'Iterations' )
+    plt.title( 'Mean squared error for all the tools over iterations' )
+    plt.grid( True )
+    plt.show()
+
+
+plot_average_cost()
+
+#plot_gradient_drop( "data/actual_gd_tools.json" ) 
+       
+'''plot_weights_distribution( "data/optimal_weights.json", "Distribution of weights for paragraph vectors approach" )
+plot_doc_tokens_mat( "data/doc_vecs_io.json", "data/doc_vecs_nd.json", "data/doc_vecs_ht.json" )
+extract_correlation( "data/similarity_scores_sources_optimal.json", "Similarity matrices for paragraph vectors approach" )'''
 plot_lr_drop( "data/learning_rates.json" )
-#plot_average_cost_low_rank()
-#plot_tokens_size()
-#plot_singular_values()
-#plot_rank_singular_variation()
-#plot_doc_tokens_mat( "data/0.1/similarity_source_orig.json" )
-#plot_doc_tokens_mat_low_rank( "data/0.1/similarity_source_low_rank.json" )
-#plot_rank_singular_variation()
-#plot_frobenius_error()'''
--- viz/js/similarity-tool.js
+++ viz/js/similarity-tool.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
                 // make html for the selected tool
                 $el_tools.append( createHTML( [ toolResults.root_tool ], selectedToolId, "Selected tool: <b>" +  selectedToolId + "</b>", "", true ) );
                 // show optimal weights
-                //$el_tools.append( showWeights( toolResults.optimal_weights, "" ) );
+                $el_tools.append( showWeights( toolResults.optimal_weights, "" ) );
                 
                 // make html for similar tools found by optimizing similarity scores using Gradient Descent
                 $el_tools.append( createHTML( toolScores, selectedToolId, "Similar tools for the selected tool: <b>" +  selectedToolId + " </b>found by optimal combination (Gradient Descent) of similarity scores from multiple sources</h4>", "Weighted similarity score", false ) );
